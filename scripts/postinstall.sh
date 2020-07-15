69a2e417777edbd19ab60bd04fab59404ba4f695
Paul Tibbetts <code@paultibbetts.uk>
2020-07-15 02:40:49 +0100

    fix(postinstall): install-peerdeps not working on yarn

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # install peer dependencies
-cd $INIT_CWD && npx install-peerdeps @ptibbetts/conventional-commits-starter --only-peers
+if [[ -f  "$INIT_CWD/yarn.lock" ]]
+then
+    cd $INIT_CWD && npx install-peerdeps @ptibbetts/conventional-commits-starter --only-peers --yarn
+else
+    cd $INIT_CWD && npx install-peerdeps @ptibbetts/conventional-commits-starter --only-peers
+fi
 
 # setup conventional commits
 commitizen init cz-conventional-changelog --save-dev --save-exact
